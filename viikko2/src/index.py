e4b68f34f49d5fec07490f73091e2f23ebd41de7
alexalgrund <alexander.algrund@gmail.com>
2025-11-10 20:54:44 +0200

    Pylint control for files.

--- viikko2/src/index.py
+++ viikko2/src/index.py
@@ -1,58 +1,61 @@
+import re
 import requests
-from player import Player
 from rich.console import Console
 from rich.table import Table
-import re
+from player import Player
+
 
 class PlayerReader:
-    def __init__(self, url):
+    def __init__(self, url: str):
         self.url = url
 
-    def get_players(self):
-        response = requests.get(self.url).json()
-        players = []
+    def get_players(self) -> list[Player]:
+        response = requests.get(self.url, timeout=10).json()
+        players_list = []
 
         for p in response:
-            player = Player(
+            pl = Player(
                 name=p.get("name"),
                 nationality=p.get("nationality"),
                 team=p.get("team"),
                 goals=p.get("goals"),
                 assists=p.get("assists")
             )
-            players.append(player)
+            players_list.append(pl)
 
-        return players
+        return players_list
+
 
 class PlayerStats:
-    def __init__(self, reader):
+    def __init__(self, reader: PlayerReader):
         self.reader = reader
 
-    def top_scorers_by_nationality(self, nationality):
-        players = self.reader.get_players()
-        filtered = [player for player in players if player.nationality == nationality]
+    def top_scorers_by_nationality(self, nation: str) -> list[Player]:
+        all_players = self.reader.get_players()
+        filtered = [p for p in all_players if p.nationality == nation]
         filtered.sort(key=lambda p: p.name)
         return filtered
 
 
 if __name__ == "__main__":
-    pattern_season = r"^\d{4}-\d{2}$"
-    season = input("Choose season: ")
-    if not re.match(pattern_season, season):
+    PATTERN_SEASON = r"^\d{4}-\d{2}$"
+    selected_season = input("Choose season: ")
+    if not re.match(PATTERN_SEASON, selected_season):
         print("Selected season is invalid.")
 
-    pattern_nationality = r"^[A-Z]{3}$"
-    nationality = input("Choose nationality: ")
-    if not re.match(pattern_nationality, nationality):
+    PATTERN_NATIONALITY = r"^[A-Z]{3}$"
+    selected_nationality = input("Choose nationality: ")
+    if not re.match(PATTERN_NATIONALITY, selected_nationality):
         print("Selected nationality is invalid.")
 
-    url = f"https://studies.cs.helsinki.fi/nhlstats/{season}/players"
-    reader = PlayerReader(url)
-    stats = PlayerStats(reader)
-    players = stats.top_scorers_by_nationality(f"{nationality}")
+    reader_instance = PlayerReader(
+        f"https://studies.cs.helsinki.fi/nhlstats/{selected_season}/players"
+    )
+    stats_instance = PlayerStats(reader_instance)
+    nation_players = stats_instance.top_scorers_by_nationality(selected_nationality)
 
     console = Console()
-    table = Table(title=f"Season {season} players from {players[0].nationality}")
+    table = Table(title=f"Season {selected_season} players from {nation_players[0].nationality}")
 
     table.add_column("Released", justify="left")
     table.add_column("Teams", justify="left")
@@ -60,11 +63,13 @@ if __name__ == "__main__":
     table.add_column("Assists", justify="right")
     table.add_column("Points", justify="right")
 
-
-
-    print(f"\n\n")
-
-    for player in players:
-        table.add_row(player.name, player.team, str(player.goals), str(player.assists), str(player.goals + player.assists))
+    for pl in nation_players:
+        table.add_row(
+            pl.name,
+            pl.team,
+            str(pl.goals),
+            str(pl.assists),
+            str(pl.goals + pl.assists)
+        )
 
     console.print(table)
